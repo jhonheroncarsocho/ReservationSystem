cc9cab9e418faf157f6cf6a7d0d8d3edf3675793
tickets14 <53848311+tickets14@users.noreply.github.com>
2021-05-31 12:42:19 +0800

    Update nav_screen.py

--- libs/baseclass/nav_screen.py
+++ libs/baseclass/nav_screen.py
@@ -3,8 +3,10 @@ from kivy.uix.screenmanager import Screen
 from kivy.lang.builder import Builder
 from kivymd.uix.label import MDLabel
 from kivy.properties import StringProperty, NumericProperty, ObjectProperty
+from kivymd_extensions.akivymd import *
 
 Builder.load_file('./libs/kv/nav_screen.kv')
+
 
 class NavLayout(Screen):
     def __init__(self, **kwargs):
