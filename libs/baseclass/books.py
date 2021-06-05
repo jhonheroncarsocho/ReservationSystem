c366bc7f7ead64f10dd9418cadee2d861292a631
tickets14 <53848311+tickets14@users.noreply.github.com>
2021-06-06 00:33:17 +0800

    Update books.py
    Changing the structure of db command

--- libs/baseclass/books.py
+++ libs/baseclass/books.py
@@ -22,7 +22,32 @@ class BookCard(MDCard):
     price = StringProperty('')
     icon = StringProperty()
     title = StringProperty()
-    count = NumericProperty(0)
+    category = StringProperty('')
+
+    def to_cart(self):
+        conn = sqlite3.connect('./assets/data/app_data.db')
+        cursor = conn.cursor()
+        cursor.execute("""CREATE TABLE IF NOT EXISTS cart(
+        id INTEGER UNIQUE PRIMARY KEY AUTOINCREMENT, 
+        product_id INTEGER, 
+        name TEXT, 
+        price INTEGER, 
+        stocks INTEGER, 
+        count INTEGER, 
+        category TEXT)""")
+        cursor.execute(f'SELECT * FROM cart WHERE product_id = {self.index}')
+        get_product = cursor.fetchone()
+        if get_product is None:
+            insert = 'INSERT INTO cart (product_id, name, price, stocks, count, category) VALUES (?,?,?,?,?,?)'
+            cursor.execute(insert, (self.index, self.name, self.price,  self.stocks, 1, self.category))
+        else:
+            cursor.execute(f'SELECT count FROM cart WHERE product_id = {self.index}')
+            get_count = cursor.fetchone()
+            if get_count[0] != self.stocks:
+                cursor.execute(f'UPDATE cart SET count = {get_count[0] + 1} WHERE product_id = {self.index}')
+
+        conn.commit()
+        conn.close()
 
 
 class Books(Screen):
@@ -38,7 +63,7 @@ class Books(Screen):
         async def on_enter():
             for info in data_items:
                 await asynckivy.sleep(0)
-                store_widgets = BookCard(index=info[0], name=info[1], price=info[2], stocks=info[3])
+                store_widgets = BookCard(index=info[0], name=info[1], price=info[2], stocks=info[3], category=info[4])
                 
                 self.ids.content.add_widget(store_widgets)
 
@@ -65,11 +90,7 @@ class Books(Screen):
 
         return data_items  # data_items
 
-
     def on_leave(self, *args):
         self.ids.content.clear_widgets()
 
-
-    def on_leave(self, *args):
-        self.ids.content.clear_widgets()
 
